3ed6c9ed8ffc3d609e42f808c344c01f3670075d
Gregory Travis <greg.m.travis@gmail.com>
2023-11-05 14:12:49 -0500

    builds

--- pedal.cpp
+++ pedal.cpp
@@ -8,19 +8,25 @@ DaisySeed hw;
 
 extern "C" {
     void rust_function();
+    void rust_function2(const float *const in_ptr, float *out_ptr, size_t len);
 }
 
 void foo() {
 	rust_function();
 }
 
+//pub const unsafe fn from_raw_parts<'a, T>(data: *const T, len: usize) -> &'a [T]
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
+  printf("size_t %d\n", sizeof(size_t));
+  rust_function2((const float*) in, (float*) out, size);
+  /*
 	for (size_t i = 0; i < size; i++)
 	{
 		out[0][i] = in[0][i];
 		out[1][i] = in[1][i];
 	}
+        */
 }
 
 int main(void)
--- step/src/lib.rs
+++ step/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use core::slice;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -8,3 +10,25 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn rust_function() {
 }
+
+#[no_mangle]
+pub extern "C" fn rust_function2(in_ptr: *const f32, out_ptr: *mut f32, len: usize) {
+  //println!("usize {}", sizeof(usize));
+  let input_slice = unsafe { slice::from_raw_parts(in_ptr, len) };
+  let output_slice = unsafe { slice::from_raw_parts_mut(out_ptr, len) };
+  lala(input_slice, output_slice, len);
+}
+
+fn lala(input_slice: &[f32], output_slice: &mut [f32], size: usize) {
+    assert!((size%2) == 0);
+    let half = size/2;
+    for i in 0..half {
+        output_slice[i] = input_slice[i+half];
+        output_slice[i+half] = input_slice[i];
+    }
+}
+
+//fn send_audio(input_slice: &[f32], output_slice: &mut [f32], size: usize) {
+
+    //typedef const float* const* InputBuffer;
+    //typedef float** OutputBuffer;
