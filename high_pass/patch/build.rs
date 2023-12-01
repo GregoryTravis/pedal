e06ed5a386d23db7ba74d762f457ff084813243f
Gregory Travis <greg.m.travis@gmail.com>
2023-12-01 00:48:43 -0500

    using dash l convention, now just missing __aeabi_atexit

--- high_pass/patch/build.rs
+++ high_pass/patch/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-search=/Users/gmt/pedal/high_pass/build/");
+    println!("cargo:rustc-link-lib=static=high_pass");
+    println!("cargo:rustc-link-search=native=/Users/gmt/pedal/high_pass/build"); // I believe this linker option is the problem
 }
