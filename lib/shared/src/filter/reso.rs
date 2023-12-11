20a07d5a179fd5ef216f9d566ff7365812dc4517
Gregory Travis <greg.m.travis@gmail.com>
2023-12-10 23:02:03 -0500

    time-varying reso

--- lib/shared/src/filter/reso.rs
+++ lib/shared/src/filter/reso.rs
@@ -1,26 +1,31 @@
 // https://www.musicdsp.org/en/latest/Filters/29-resonant-filter.html
 
+extern crate libm;
+
 pub struct ResoFilter {
   pub buf0: f32,
   pub buf1: f32,
-  pub f: f32,
-  pub fb: f32,
+  pub q: f32,
 }
 
 impl ResoFilter {
   pub fn new() -> ResoFilter {
-    let f = 0.7;
     let q = 0.95;
-    let fb = q + q/(1.0 - f);
-    ResoFilter { buf0: 0.0, buf1: 0.0, f: f, fb: fb }
+    ResoFilter { buf0: 0.0, buf1: 0.0, q: q }
   }
 
   pub fn filter(&mut self, input_slice: &[f32], output_slice: &mut [f32], size: usize,
-                _time_in_seconds: f64) {
+                time_in_seconds: f64) {
     for i in 0..size {
+      // Rolls over every 68 years
+      let osc = libm::sinf(time_in_seconds as f32);
+      let max_f = 0.9;
+      let min_f = 0.3;
+      let oscf = min_f + ((max_f-min_f) * ((osc+1.0)/2.0));
+      let fb = self.q + self.q/(1.0 - oscf);
       let inp = input_slice[i];
-      self.buf0 = self.buf0 + self.f * (inp - self.buf0 + self.fb * (self.buf0 - self.buf1));
-      self.buf1 = self.buf1 + self.f * (self.buf0 - self.buf1);
+      self.buf0 = self.buf0 + oscf * (inp - self.buf0 + fb * (self.buf0 - self.buf1));
+      self.buf1 = self.buf1 + oscf * (self.buf0 - self.buf1);
       let out = self.buf1;
       output_slice[i] = out;
     }
