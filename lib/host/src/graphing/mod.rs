ed99015da174f5fee5136979262cdc5872695b61
Gregory Travis <greg.m.travis@gmail.com>
2023-12-23 00:16:52 -0500

    cleanup

--- lib/host/src/graphing/mod.rs
+++ lib/host/src/graphing/mod.rs
@@ -8,10 +8,6 @@ pub fn graph2dfun(width: u32, height: u32, x_range: Range<f32>, y_range: Range<f
 
     root_area.fill(&WHITE)?;
 
-    //let root_area = root_area.titled("Image Title", ("sans-serif", 60))?;
-
-    //let (upper, lower) = root_area.split_vertically(512);
-
     let step: f32 = (x_range.end - x_range.start) / (width as f32);
     let x_axis = x_range.clone().step(step);
 
@@ -33,22 +29,7 @@ pub fn graph2dfun(width: u32, height: u32, x_range: Range<f32>, y_range: Range<f
         .label("Sine")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
 
-    cc.draw_series(LineSeries::new(
-        x_axis.values().map(|x| (x, x.cos())),
-        &BLUE,
-    ))?
-    .label("Cosine")
-    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
-
     cc.configure_series_labels().border_style(BLACK).draw()?;
-
-    /*
-    // It's possible to use a existing pointing element
-     cc.draw_series(PointSeries::<_, _, Circle<_>>::new(
-        (-3.0f32..2.1f32).step(1.0).values().map(|x| (x, x.sin())),
-        5,
-        Into::<ShapeStyle>::into(&RGBColor(255,0,0)).filled(),
-    ))?;*/
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root_area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
