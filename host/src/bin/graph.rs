2aed4280469b94a48b121c2e9690c38b08618edb
Gregory Travis <greg.m.travis@gmail.com>
2023-12-23 09:44:11 -0500

    3d

--- host/src/bin/graph.rs
+++ host/src/bin/graph.rs
@@ -2,6 +2,8 @@ use host::graphing::*;
 
 pub fn main() {
   let fun: fn(f32)-> f32 = |x| (x*10.0).sin();
-  graph2dfun(1024, 768, -3.4f32..3.4, -1.2f32..1.2f32, fun).unwrap();
+  graph_2d_fun("out2d.png", 1024, 768, -3.4f32..3.4, -1.2f32..1.2f32, fun).unwrap();
+  graph_3d_line_fun("out3d.svg", 1024, 768, -3.0..3.0, -3.0..3.0, -3.0..3.0,
+                    |t| (t*2.0, (t*100.0).sin(), (t*100.0).cos()), 500).unwrap();
 }
 
--- lib/host/src/graphing/mod.rs
+++ lib/host/src/graphing/mod.rs
@@ -2,9 +2,8 @@ use std::ops::Range;
 
 use plotters::prelude::*;
 
-const OUT_FILE_NAME: &str = "plotters-doc-data/sample.png";
-pub fn graph2dfun(width: u32, height: u32, x_range: Range<f32>, y_range: Range<f32>, fun: fn(f32) -> f32) -> Result<(), Box<dyn std::error::Error>> {
-    let root_area = BitMapBackend::new(OUT_FILE_NAME, (width, height)).into_drawing_area();
+pub fn graph_2d_fun(filename: &str, width: u32, height: u32, x_range: Range<f32>, y_range: Range<f32>, fun: fn(f32) -> f32) -> Result<(), Box<dyn std::error::Error>> {
+    let root_area = BitMapBackend::new(filename, (width, height)).into_drawing_area();
 
     root_area.fill(&WHITE)?;
 
@@ -33,6 +32,47 @@ pub fn graph2dfun(width: u32, height: u32, x_range: Range<f32>, y_range: Range<f
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root_area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
-    println!("Result has been saved to {}", OUT_FILE_NAME);
+    println!("Result has been saved to {}", filename);
+    Ok(())
+}
+
+pub fn graph_3d_line_fun(filename: &str, width: u32, height: u32, x_range: Range<f32>, y_range: Range<f32>, z_range: Range<f32>, fun: fn(f32) -> (f32, f32, f32), num_points: u32) -> Result<(), Box<dyn std::error::Error>> {
+    let area = SVGBackend::new(filename, (width, height)).into_drawing_area();
+
+    area.fill(&WHITE)?;
+
+    let x_axis = x_range.step(0.1);
+    let y_axis = y_range.step(0.1);
+    let z_axis = z_range.step(0.1);
+
+    let mut chart = ChartBuilder::on(&area)
+        .caption("3D Plot Test".to_string(), ("sans", 20))
+        .build_cartesian_3d(x_axis.clone(), y_axis.clone(), z_axis.clone())?;
+
+    chart.with_projection(|mut pb| {
+        pb.yaw = 0.5;
+        pb.scale = 0.9;
+        pb.into_matrix()
+    });
+
+    chart
+        .configure_axes()
+        .light_grid_style(BLACK.mix(0.15))
+        .max_light_lines(3)
+        .draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            (0.0f32..1.0f32).step(1.0 / (num_points as f32)).values().map(fun),
+            &BLACK,
+        ))?
+        .label("Line")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLACK));
+
+    chart.configure_series_labels().border_style(BLACK).draw()?;
+
+    // To avoid the IO failure being ignored silently, we manually call the present function
+    area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+    println!("Result has been saved to {}", filename);
     Ok(())
 }
