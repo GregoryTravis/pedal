9332cb98140f9a1e0bad19e6d84be9e089f2ea88
Gregory Travis <greg.m.travis@gmail.com>
2023-12-23 10:16:05 -0500

    canonical_complex_sinusoid

--- host/src/bin/graph.rs
+++ host/src/bin/graph.rs
@@ -1,9 +1,14 @@
+use std::f32::consts::PI;
+
 use host::graphing::*;
 
 pub fn main() {
   let fun: fn(f32)-> f32 = |x| (x*10.0).sin();
   graph_2d_fun("out2d.png", 1024, 768, -3.4f32..3.4, -1.2f32..1.2f32, fun).unwrap();
+  let canonical_complex_sinusoid = |t: f32| {
+    let tt = t*2.0*PI;
+    (tt.cos(), tt.sin(), tt)
+  };
   graph_3d_line_fun("out3d.svg", 1024, 768, -3.0..3.0, -3.0..3.0, -3.0..3.0,
-                    |t| (t*2.0, (t*100.0).sin(), (t*100.0).cos()), 500).unwrap();
+                    canonical_complex_sinusoid, 500).unwrap();
 }
-
