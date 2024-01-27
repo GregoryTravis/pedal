221e10f6ad4e1bccc2e4c0cf6915da1ae37064f2
Gregory Travis <greg.m.travis@gmail.com>
2024-01-27 17:16:09 -0500

    Fix tremelo crash by using epsilon instead of 0.0 comparison in sinc().

--- lib/shared/src/filter/tremolo.rs
+++ lib/shared/src/filter/tremolo.rs
@@ -27,7 +27,8 @@ const NOW_INDEX: usize = MAX_SAMPLE_DEVIATION + NUM_SINC_TAPS_ONE_SIDE + GUARD_S
 const TREMOLO_FREQUENCY: f32 = 1.0;
 
 fn sinc(x: f32) -> f32 {
-    if x == 0.0 {
+    let small = 0.000000001;
+    if x < small || x > -small {
         1.0
     } else {
         libm::sinf(x) / x
