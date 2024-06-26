a02fcbfa8e4f05dff04d934e7c5bb5c0ea215d83
Gregory Travis <greg.m.travis@gmail.com>
2024-06-26 19:42:05 -0400

    sanity_check from init

--- board/src/daisy_seed.rs
+++ board/src/daisy_seed.rs
@@ -15,16 +15,20 @@ pub fn hw_get_size_t_size() -> usize {
         cpp_hw_get_size_t_size()
     }
 }
-pub fn hw_sanity_check() {
+
+fn hw_sanity_check() {
     // The irony here is not lost on me.
     let size_t_size = hw_get_size_t_size();
     assert!(size_t_size == size_of::<usize>());
 }
 
 pub fn hw_init(b: bool, block_size: usize) {
+    hw_sanity_check();
+
     unsafe {
         cpp_hw_init(b, block_size);
     }
+
     if KSHEP {
         unsafe {
             cpp_hw_kshep_init();
--- board/src/rustmain.rs
+++ board/src/rustmain.rs
@@ -15,8 +15,6 @@ use shared::spew::*;
 
 #[no_mangle]
 pub fn main() {
-    hw_sanity_check();
-
     let siner = PostCompose { signal: Arc::new(Sin {}), ff: scale_range(0.3, 0.9) };
     let q = Const { x: 0.95 };
     rig_install_patch(Box::new(ResoFilter::new(Arc::new(siner), Arc::new(q))));
