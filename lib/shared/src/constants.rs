b3f590123dccfb576842f424ff4e8d150598cb57
Gregory Travis <greg.m.travis@gmail.com>
2024-06-26 18:18:22 -0400

    comments

--- lib/shared/src/constants.rs
+++ lib/shared/src/constants.rs
@@ -1,3 +1,5 @@
+// TODO: generate these from a shared config.
+// TODO: use sample rate enum
 pub const SAMPLE_RATE: u32 = 48000;
 pub const BLOCK_SIZE: usize = 48;
 pub const KSHEP: bool = true;
--- lib/shim/constants.h
+++ lib/shim/constants.h
@@ -3,6 +3,7 @@
 
 #include "daisy_seed.h"
 
+// TODO: generate these from a shared config.
 #define SAMPLE_RATE 48000 // SaiHandle::Config::SampleRate::SAI_48KHZ
 #define BLOCK_SIZE 48
 
