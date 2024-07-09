46526924baf6f98370a189ff888a4b0e2b5a75de
Gregory Travis <greg.m.travis@gmail.com>
2024-07-09 15:25:41 -0400

    cleanup

--- lib/shared/src/rig_util.rs
+++ lib/shared/src/rig_util.rs
@@ -17,17 +17,12 @@ pub fn rig_run_patch_on_buffer(patch: Box<dyn Patch>, input: &[f32], output: &mu
         let start = sofar;
         let end = min(sofar + BLOCK_SIZE, len);
         let block_length = end-start;
-        //println!("rpob {} {} {} {}", sofar, len, start, end);
         let sub_input = &input[start..end];
         let mut sub_output: &mut [f32] = &mut output[start..end];
-//let body_slice: &mut [u8] = &mut myvec[10..1034];
 
-        //println!("rpob {} {}", sub_input.len(), sub_output.len());
         rust_process_audio_soft(&sub_input, &mut sub_output, block_length);
-        //println!("rpob 2 {} {}", sub_input.len(), sub_output.len());
 
         sofar += BLOCK_SIZE;
-        //println!("rpob hey");
     }
 
     rig_deinstall_patch();
