82c82489e40330d38a8f8bbf19093c60a2c4b490
Gregory Travis <greg.m.travis@gmail.com>
2024-07-14 21:23:12 -0400

    spew in panic

--- lib/shared/src/panic.rs
+++ lib/shared/src/panic.rs
@@ -1,5 +1,9 @@
+use alloc::string::ToString;
+use crate::spew::*;
+
 #[cfg(not(feature = "for_host"))]
 #[cfg_attr(not(feature = "for_host"), panic_handler)]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    spew!(info.to_string().as_str());
     loop {}
 }
