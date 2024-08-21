8975487c2ceebc3f07a7716dac6248ab30623b53
Gregory Travis <greg.m.travis@gmail.com>
2024-08-20 23:12:15 -0400

    more spew

--- lib/shim/fft.cpp
+++ lib/shim/fft.cpp
@@ -29,13 +29,21 @@ extern "C" void do_fft() {
     in[i] = ((float32_t)i) / ((float32_t)SIZE);
   }
 
+  hw.PrintLine("AAA before");
   for (size_t i = 0; i < SIZE; ++i) {
     hw.PrintLine("%d %f %f %f\n", i, in[i], fftBuffer[i], out[i]);
   }
 
   arm_rfft_fast_f32(fftInstance, in, fftBuffer, 0);
+
+  hw.PrintLine("AAA after fft");
+  for (size_t i = 0; i < SIZE; ++i) {
+    hw.PrintLine("%d %f %f %f\n", i, in[i], fftBuffer[i], out[i]);
+  }
+
   arm_rfft_fast_f32(fftInstance, fftBuffer, out, 1);
 
+  hw.PrintLine("AAA after ifft");
   for (size_t i = 0; i < SIZE; ++i) {
     hw.PrintLine("%d %f %f %f\n", i, in[i], fftBuffer[i], out[i]);
   }
