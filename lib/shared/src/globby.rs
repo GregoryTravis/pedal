66db4fc514eb2d2cfafc4b6527b9e130817a1d29
Gregory Travis <greg.m.travis@gmail.com>
2024-10-10 08:34:28 -0400

    shared set clear use_thing

--- lib/shared/src/globby.rs
+++ lib/shared/src/globby.rs
@@ -2,3 +2,41 @@
 pub use crate::globby_board::*;
 #[cfg(feature = "for_host")]
 pub use crate::globby_host::*;
+
+impl <T> Globby<T> {
+    pub fn set(&self, thing: T) {
+        //interrupt::free(|cs| self.thing.borrow(cs).replace(Some(thing)));
+        self.lala(|mor| {
+            *mor = Some(thing);
+        });
+    }
+
+    pub fn clear(&self) {
+        //interrupt::free(|cs| { self.thing.borrow(cs).replace(None); });
+        self.lala(|mor| {
+            *mor = None;
+        });
+    }
+
+    pub fn use_thing<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R {
+        self.lala(|mor| {
+            if let Some(ref mut thing) = mor.as_mut() {
+                f(thing)
+            } else {
+                todo!();
+            }
+            //f(mor.as_mut())
+        })
+        /*
+        interrupt::free(|cs| {
+            if let Some(ref mut thing) = self.thing.borrow(cs).borrow_mut().deref_mut().as_mut() {
+                f(thing)
+            } else {
+                todo!()
+            }
+        })
+        */
+    }
+}
--- lib/shared/src/globby_board.rs
+++ lib/shared/src/globby_board.rs
@@ -19,43 +19,7 @@ impl <T> Globby<T> {
         }
     }
 
-    pub fn set(&self, thing: T) {
-        //interrupt::free(|cs| self.thing.borrow(cs).replace(Some(thing)));
-        self.lala(|mor| {
-            *mor = Some(thing);
-        });
-    }
-
-    pub fn clear(&self) {
-        //interrupt::free(|cs| { self.thing.borrow(cs).replace(None); });
-        self.lala(|mor| {
-            *mor = None;
-        });
-    }
-
-    pub fn use_thing<F, R>(&self, f: F) -> R
-    where
-        F: FnOnce(&mut T) -> R {
-        self.lala(|mor| {
-            if let Some(ref mut thing) = mor.as_mut() {
-                f(thing)
-            } else {
-                todo!();
-            }
-            //f(mor.as_mut())
-        })
-        /*
-        interrupt::free(|cs| {
-            if let Some(ref mut thing) = self.thing.borrow(cs).borrow_mut().deref_mut().as_mut() {
-                f(thing)
-            } else {
-                todo!()
-            }
-        })
-        */
-    }
-
-    fn lala<F, R>(&self, f: F) -> R
+    pub fn lala<F, R>(&self, f: F) -> R
     where
         F: FnOnce(&mut Option<T>) -> R {
         interrupt::free(|cs| {
