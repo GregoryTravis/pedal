02ed5251b84cd6581169acbab4a96ad252513d42
Gregory Travis <greg.m.travis@gmail.com>
2024-12-14 14:13:51 -0500

    B is always the same

--- lib/shared/src/edsl/runtime/prim.rs
+++ lib/shared/src/edsl/runtime/prim.rs
@@ -1,12 +1,12 @@
 use crate::edsl::runtime::cursor::Cursor;
 
-pub fn pass_thru<const PI: usize, const FI: usize, const BI: usize, const TI: usize, const PO: usize,
-                 const FO: usize, const BO: usize, const TO: usize>(i: usize, inc: &Cursor<PI, FI, BI, TI>, outc: &mut Cursor<PO, FO, BO, TO>) {
+pub fn pass_thru<const PI: usize, const FI: usize, const B: usize, const TI: usize, const PO: usize,
+                 const FO: usize, const TO: usize>(i: usize, inc: &Cursor<PI, FI, B, TI>, outc: &mut Cursor<PO, FO, B, TO>) {
     outc.write(i, inc.read(i as isize));
 }
 
-pub fn add<const PI: usize, const FI: usize, const BI: usize, const TI: usize,
-           const PI2: usize, const FI2: usize, const BI2: usize, const TI2: usize,
-           const PO: usize, const FO: usize, const BO: usize, const TO: usize>(i: usize, inc: &Cursor<PI, FI, BI, TI>, in2c: &Cursor<PI2, FI2, BI2, TI2>, outc: &mut Cursor<PO, FO, BO, TO>) {
+pub fn add<const PI: usize, const FI: usize, const B: usize, const TI: usize,
+           const PI2: usize, const FI2: usize, const TI2: usize,
+           const PO: usize, const FO: usize, const TO: usize>(i: usize, inc: &Cursor<PI, FI, B, TI>, in2c: &Cursor<PI2, FI2, B, TI2>, outc: &mut Cursor<PO, FO, B, TO>) {
     outc.write(i, inc.read(i as isize) + in2c.read(i as isize));
 }
