a02d2b38757c1d696263383a9133f1166bfd13b9
Gregory Travis <greg.m.travis@gmail.com>
2024-12-14 15:18:07 -0500

    tr union, combine claims

--- lib/shared/src/edsl/wad/node.rs
+++ lib/shared/src/edsl/wad/node.rs
@@ -1,7 +1,9 @@
 extern crate std;
 
+//use std::collections::HashMap;
 use std::cmp::{min,max};
 use alloc::rc::Rc;
+use alloc::vec::Vec;
 
 #[derive(Copy, Clone, Debug)]
 pub struct TimeRange {
@@ -11,7 +13,15 @@ pub struct TimeRange {
 
 #[derive(Copy, Clone, Debug)]
 pub struct NodeInfo {
+// TODO remove unuseds
+#[allow(unused)]
     index: usize,
+    time_range: TimeRange,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct BufferInfo {
+#[allow(unused)]
     time_range: TimeRange,
 }
 
@@ -20,6 +30,14 @@ pub fn tr_union(a: &TimeRange, b: &TimeRange) -> TimeRange {
         past: min(a.past, b.past),
         future: max(a.future, b.future),
     }
+}
+
+pub fn tr_unions(trs: Vec<TimeRange>) -> TimeRange {
+    let mut tr = trs[0];
+    for tr2 in &trs[1..] {
+        tr = tr_union(&tr, tr2);
+    }
+    tr
 }
 
 pub fn n_union(a: &NodeInfo, b: &NodeInfo) -> (NodeInfo, NodeInfo) {
@@ -32,9 +50,24 @@ pub fn n_union(a: &NodeInfo, b: &NodeInfo) -> (NodeInfo, NodeInfo) {
 #[derive(Debug)]
 pub enum Node {
     Input(NodeInfo),
-    //PassThru { n: Rc<Node> },
     PassThru(NodeInfo, Rc<Node>),
     Add(NodeInfo, Rc<Node>, Rc<Node>),
+}
+
+fn get_node_info(n: &Rc<Node>) -> NodeInfo {
+    match **n {
+        Node::Input(ni) => ni,
+        Node::PassThru(ni, _) => ni,
+        Node::Add(ni, _, _) => ni,
+    }
+}
+
+fn self_and_srcs(n: Rc<Node>) -> Vec<Rc<Node>> {
+    match &*n {
+        Node::Input(_) => vec![n],
+        Node::PassThru(_, c) => vec![n.clone(), c.clone()],
+        Node::Add(_, a, b) => vec![n.clone(), a.clone(), b.clone()],
+    }
 }
 
 pub fn trn(past: usize, future: usize) -> NodeInfo {
@@ -45,4 +78,10 @@ pub fn trn(past: usize, future: usize) -> NodeInfo {
             future: future,
         },
     }
+}
+
+pub fn combine_claims(n: Rc<Node>) -> TimeRange {
+    let trs: Vec<TimeRange> = self_and_srcs(n).iter().map(|n| get_node_info(n).time_range).collect();
+    let tr = tr_unions(trs);
+    tr
 }
