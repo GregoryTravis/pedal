a72509072251b82b7780e0624a046636cf914e42
Gregory Travis <greg.m.travis@gmail.com>
2024-12-30 13:01:29 -0500

    fix build

--- lib/shared/src/fft_bench.rs
+++ lib/shared/src/fft_bench.rs
@@ -10,7 +10,7 @@ pub fn do_benchmark_fft() {
     let mut output: [f32; FFT_SIZE] = [0.0; FFT_SIZE];
 
     let dur = 1.0;
-    let bench = benchmark(dur, || {
+    let bench = benchmark(dur, &mut || {
         fft(&mut input, &mut fft_buffer);
         ifft(&mut fft_buffer, &mut output);
     });
