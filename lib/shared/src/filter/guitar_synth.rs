bc659076c4d38ec46d51a6514b0426503a89a5bb
Gregory Travis <greg.m.travis@gmail.com>
2025-03-05 23:46:33 -0500

    add reso

--- lib/shared/src/filter/guitar_synth.rs
+++ lib/shared/src/filter/guitar_synth.rs
@@ -4,13 +4,16 @@ extern crate libm;
 extern crate std;
 
 use alloc::boxed::Box;
+use core::f32::consts::PI;
 //#[cfg(feature = "for_host")]
 //use std::println;
 
 use crate::convert::*;
+use crate::constants::*;
 use crate::knob::Knobs;
 use crate::patch::Patch;
 use crate::playhead::Playhead;
+#[allow(unused)]
 use crate::spew::*;
 use crate::yin::*;
 
@@ -19,6 +22,11 @@ const BUFFER_SIZE: usize = 1024;
 pub struct GuitarSynth {
     // TODO put in sdram
     int_buffer: [i16; BUFFER_SIZE],
+    last_pitch: f32,
+
+    // Resonant filter history
+    pub buf0: f32,
+    pub buf1: f32,
 }
 
 impl GuitarSynth {
@@ -26,6 +34,9 @@ impl GuitarSynth {
         yin_init(BUFFER_SIZE as i16, 0.05);
         GuitarSynth {
             int_buffer: [0; BUFFER_SIZE],
+            last_pitch: 440.0,
+            buf0: 0.0,
+            buf1: 0.0,
         }
     }
 }
@@ -34,7 +45,7 @@ impl Patch for GuitarSynth {
     fn rust_process_audio(
         &mut self,
         input_slice: &[f32],
-        _output_slice: &mut [f32],
+        output_slice: &mut [f32],
         _knobs: &Box<dyn Knobs>,
         mut playhead: Playhead,
     ) {
@@ -45,15 +56,30 @@ impl Patch for GuitarSynth {
             self.int_buffer[i] = self.int_buffer[i+slice_len];
         }
         for i in 0..slice_len {
-            //spew!("um", input_slice[i]);
             self.int_buffer[i+BUFFER_SIZE-slice_len] = sample_f32_to_i16(input_slice[i]) >> 6;
         }
 
-        let pitch: f32 = yin_process(&self.int_buffer);
+        let pitch_maybe: f32 = yin_process(&self.int_buffer);
+        let pitch = if pitch_maybe == -1.0 { self.last_pitch } else { pitch_maybe };
+        self.last_pitch = pitch;
 
-        spew!("pitch", pitch);
+        // spew!("pitch", pitch);
 
-        // TODO: need this?
-        playhead.inc();
+        let oscf: f32 = 2.0 * libm::sinf(PI * (pitch / SAMPLE_RATE as f32));
+
+        //let q_lo = 0.4;
+        //let q_hi = 0.99;
+        let q = 0.97f32;
+
+        for i in 0..input_slice.len() {
+            //let _qq = add(&self.q_sig, &self.q_sig);
+            let fb = q + q / (1.0 - oscf);
+            let inp = input_slice[i];
+            self.buf0 = self.buf0 + oscf * (inp - self.buf0 + fb * (self.buf0 - self.buf1));
+            self.buf1 = self.buf1 + oscf * (self.buf0 - self.buf1);
+            let out = self.buf1;
+            output_slice[i] = out;
+            playhead.inc();
+        }
     }
 }
