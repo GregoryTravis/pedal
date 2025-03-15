9cc1deef46fad3406984374d7d9a9b8843977075
Gregory Travis <greg.m.travis@gmail.com>
2025-03-15 18:06:39 -0400

    build.rs

---
+++ lib/shared/build.rs
@@ -0,0 +1,10 @@
+use cc;
+
+fn main() {
+    //std::env::set_var("TARGET", "arm64-apple-darwin");
+    cc::Build::new()
+        .file("src/yin/Yin.c")
+        .compile("yin");
+    println!("cargo:rustc-link-lib=yin");
+    println!("cargo:rerun-if-changed=src/yin");
+}
