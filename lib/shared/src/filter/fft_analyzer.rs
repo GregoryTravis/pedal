d3ffee4c3b31378f06908bbad492f33adcb1f3a0
Gregory Travis <greg.m.travis@gmail.com>
2025-03-15 23:43:03 -0400

    quadratic interpolation

--- lib/shared/src/filter/fft_analyzer.rs
+++ lib/shared/src/filter/fft_analyzer.rs
@@ -26,6 +26,22 @@ impl FFTAnalyzer {
     }
 }
 
+// Return peak of the curve described by the values.
+// Returns (x_peak, y_peak).
+// x_peak is relative to xp which is treated as 0.
+// https://www.physics.drexel.edu/~steve/Courses/Comp_Phys/Physics-105/quad_int.pdf
+fn quadratic_interpolate(xpp: f32, xp: f32, x: f32) -> (f32, f32) {
+    // Remove mult by 1 and other stupid things.
+    let dt = 1.0;
+    let tp = 0.0;
+    let a = xp;
+    let b = (x - xpp) / (2.0 * dt);
+    let c = (x - (2.0 * xp) + xpp) / (2.0 * dt * dt);
+    let tau = tp - (b / (2.0 * c));
+    let x_max = a - ((b * b) / (4.0 * c));
+    (tau, x_max)
+}
+
 impl Patch for FFTAnalyzer {
     fn rust_process_audio(
         &mut self,
@@ -55,6 +71,21 @@ impl Patch for FFTAnalyzer {
         spew!("====");
         for i in 0..FFT_SIZE {
             spew!("fft", i, self.output[i]);
+
+            let not_edge = i > 0 && i < FFT_SIZE-1;
+            if not_edge {
+                let a = self.output[i-1];
+                let b = self.output[i];
+                let c = self.output[i+1];
+                // TODO what if they are equal
+                let is_peak = not_edge && a < b && b > c;
+                if is_peak {
+                    let (relative_x_peak, y_peak) = quadratic_interpolate(self.output[i-1], self.output[i], self.output[i+1]);
+                    let x_peak = (i as f32) + relative_x_peak;
+                    let amp_peak = y_peak / (FFT_SIZE / 2) as f32;
+                    spew!("*** peak", x_peak, y_peak, amp_peak);
+                }
+            }
         }
 
         for i in 0..input_slice.len() {
