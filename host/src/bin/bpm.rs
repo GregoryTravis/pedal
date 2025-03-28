3a66bfb6a95095a8ca0a676d43ba9e9db94130a1
Gregory Travis <greg.m.travis@gmail.com>
2025-03-27 23:27:56 -0400

    bp and bpm

--- host/src/bin/bpm.rs
+++ host/src/bin/bpm.rs
@@ -13,17 +13,21 @@ use shared::spew::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_filename = &args[1];
-    let freq = args[2].parse::<f32>().unwrap();
-    let bw = args[3].parse::<f32>().unwrap();
+    let bw = args[2].parse::<f32>().unwrap();
+
+    //let freqs: Vec<f32> = vec![172.0, 220.0, 328.0];
+    let freqs: Vec<f32> = vec![770.0];
 
     let input = file_read(input_filename);
     let mut output = vec![0.0; input.len()];
 
-    let mut bp: BandPass = BandPass::new(freq, bw);
-    for i in 0..input.len() {
-        output[i] = bp.process(input[i]);
+    for freq in freqs {
+        let mut bp: BandPass = BandPass::new(freq, bw);
+        for i in 0..input.len() {
+            output[i] += bp.process(input[i]);
+        }
     }
 
-    let output_filename = format!("{}-bp-{}-{}.wav", input_filename, freq, bw);
+    let output_filename = format!("{}-bp-{}.wav", input_filename, bw);
     file_write(&output_filename, &output);
 }
