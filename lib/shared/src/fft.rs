294eed64c62229940ac99c14529f834d0e098137
Gregory Travis <greg.m.travis@gmail.com>
2025-04-18 01:29:24 -0400

    comment about bashing input

--- lib/shared/src/fft.rs
+++ lib/shared/src/fft.rs
@@ -4,6 +4,8 @@ use crate::constants::*;
 
 // This is a goofy usage pattern, but I have two very different ffts to unify.
 pub trait FFT {
+    // Assume the implementation will bash the input.
+    // (True for microfft, not true for ARM.)
     fn get_input(&mut self) -> &mut [f32; FFT_SIZE];
     fn run(&mut self) -> &[f32; FFT_SIZE];
 }
