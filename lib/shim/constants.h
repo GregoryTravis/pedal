0ec6dacbbc631fecfaed29084a7af7e9c87caf46
Gregory Travis <greg.m.travis@gmail.com>
2025-04-23 13:39:52 -0400

    docs

--- lib/shim/constants.h
+++ lib/shim/constants.h
@@ -9,6 +9,7 @@
 #define KSHEP true
 #define PROD false
 
+// I think the 128 makes room for the rest of the SDRAM struct.
 #define SDRAM_SIZE_BYTES ((64 * 1024 * 1024) - 128)
 #define SDRAM_SIZE_F32 (SDRAM_SIZE_BYTES / sizeof(float))
 
