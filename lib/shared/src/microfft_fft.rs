ec2f76491a1e401e658aea2ce643a2d7e0ed3575
Gregory Travis <greg.m.travis@gmail.com>
2025-04-23 16:20:13 -0400

    share impl

--- lib/shared/src/microfft_fft.rs
+++ lib/shared/src/microfft_fft.rs
@@ -23,14 +23,18 @@ impl FFT for MicroFFT {
     }
 
     fn run(&mut self) -> &[f32; FFT_SIZE] {
-        // TODO cast the pointer, don't do the copy
-        let input_len = self.input.len();
-        let complex_output = rfft_2048(&mut self.input);
-        assert!(complex_output.len() * 2 == input_len);
-        for i in 0..complex_output.len() {
-            self.output[i*2] = complex_output[i].re;
-            self.output[i*2+1] = complex_output[i].im;
-        }
+        run_on_arrays(&mut self.input, &mut self.output);
         &self.output
+    }
+}
+
+pub fn run_on_arrays(input: &mut [f32; FFT_SIZE], output: &mut [f32; FFT_SIZE]) {
+    // TODO cast the pointer, don't do the copy
+    let input_len = input.len();
+    let complex_output = rfft_2048(input);
+    assert!(complex_output.len() * 2 == input_len);
+    for i in 0..complex_output.len() {
+        output[i*2] = complex_output[i].re;
+        output[i*2+1] = complex_output[i].im;
     }
 }
--- lib/shared/src/microfft_sdram_fft.rs
+++ lib/shared/src/microfft_sdram_fft.rs
@@ -1,7 +1,6 @@
-use microfft::real::*;
-
 use crate::constants::*;
 use crate::fft::*;
+use crate::microfft_fft::*;
 use crate::sdram::*;
 
 pub struct MicroFFTSDRAM<'a> {
@@ -24,15 +23,8 @@ impl <'a> FFT for MicroFFTSDRAM<'a> {
     }
 
     fn run(&mut self) -> &[f32; FFT_SIZE] {
-        // TODO cast the pointer, don't do the copy
-        let input_len = self.input.len();
-        let complex_output = rfft_2048(&mut self.input);
-        assert!(complex_output.len() * 2 == input_len);
-        for i in 0..complex_output.len() {
-            self.output[i*2] = complex_output[i].re;
-            self.output[i*2+1] = complex_output[i].im;
-        }
-        &self.output
+        run_on_arrays(self.input, self.output);
+        self.output
     }
 }
 
