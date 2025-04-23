55f47a68f462957cfba5bf51a7fc9c741b8fabc4
Gregory Travis <greg.m.travis@gmail.com>
2025-04-23 10:37:51 -0400

    quake isqrt

--- lib/shared/src/fft.rs
+++ lib/shared/src/fft.rs
@@ -1,6 +1,7 @@
 extern crate libm;
 
 use crate::constants::*;
+use crate::spew::*;
 
 // This is a goofy usage pattern, but I have two very different ffts to unify.
 pub trait FFT {
@@ -16,6 +17,28 @@ pub fn fft_to_magnitudes(fft_in: &[f32; FFT_SIZE], mag_out: &mut [f32; FFT_SIZE/
     for i in 0..FFT_SIZE/2 {
         let re = fft_in[i*2];
         let im = fft_in[i*2+1];
-        mag_out[i] = libm::sqrtf(re*re + im*im);
+        //mag_out[i] = libm::sqrtf(re*re + im*im);
+        mag_out[i] = 1.0 / quake_rsqrt(re*re + im*im);
+        //mag_out[i] = re*re + im*im;
     }
+}
+
+// https://www.reddit.com/r/rust/comments/vdroh6/i_implemented_the_famous_quake_inverse_sqrt/
+#[allow(unused)]
+fn quake_rsqrt(number: f32) -> f32 {
+    let mut y: f32 = number;
+    unsafe {
+        let mut i: i32 = core::mem::transmute::<f32, i32>(y);
+        i = 0x5F375A86 - (i >> 1);
+        y = core::mem::transmute::<i32, f32>(i);
+    }
+    y * (1.5 - (number * 0.5 * y * y))
+}
+
+#[allow(unused)]
+fn quake_rsqrt2(number: f32) -> f32 {
+    let mut i: i32 = number.to_bits() as i32;
+    i = 0x5F375A86_i32.wrapping_sub(i >> 1);
+    let y = f32::from_bits(i as u32);
+    y * (1.5 - (number * 0.5 * y * y))
 }
