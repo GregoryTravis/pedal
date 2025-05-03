c9ae017218eeb44ef943a510734e268a04925fd5
Gregory Travis <greg.m.travis@gmail.com>
2025-05-02 23:19:31 -0400

    10x4, new golden

--- lib/shared/src/hop_fft.rs
+++ lib/shared/src/hop_fft.rs
@@ -154,16 +154,16 @@ fn find_peaks(dump: bool, wid: f32, ness: f32, fft: &[f32; FFT_SIZE/2], /*out*/ 
             // spew!("peak", mark, i, ppeak, fft[i], peak_mag, peak_freq, peak_amp, sharpness, window_start, window_end, show_f, show_a);
         }
 
-        let max_funds = 8;
-        let num_overtones = 1;
+        let max_funds = 10;
+        let num_overtones = 4;
         let max_num_peaks: usize = max_funds * num_overtones; // 10 * # of overtones
         match fa {
             Some((f, _)) => {
                 if peaks.len() < max_num_peaks {
                     peaks.push(f);
-                    //peaks.push(f * 2.0);
-                    //peaks.push(f * 3.0);
-                    //peaks.push(f * 5.0);
+                    peaks.push(f * 2.0);
+                    peaks.push(f * 3.0);
+                    peaks.push(f * 5.0);
                 }
             }
             None => (),
