67d7fc97441b7a2579c1b92d11bb2e931652a5e8
Gregory Travis <greg.m.travis@gmail.com>
2025-11-05 21:41:35 -0500

    three groups

--- lib/shared/src/much_harm.rs
+++ lib/shared/src/much_harm.rs
@@ -10,19 +10,23 @@ use crate::filter::seq::*;
 use crate::patch::Patch;
 use crate::sdram::*;
 
-const TONES: [f32; 6] = [0.25, 0.5, 1.0, 1.5, 2.0, 3.0];
+const TONES: [(usize, f32); 7] =
+  [(0, 0.25), (0, 0.5),
+   (1, 2.0), (1, 4.0),
+   (2, 0.75), (2, 1.5), (2, 3.0)];
 
 pub fn much_harm(sdram: &mut SDRAM) -> Box<dyn Patch> {
     let mut harms: Vec<MixerChannel> = vec![];
-    for (index, ratio) in TONES.iter().enumerate() {
+    for (knob_num, ratio) in TONES.iter() {
         let h = if *ratio == 1.0 {
             Box::new(PassThruFilter::new()) as Box<dyn Patch>
         } else {
             Box::new(Harmoneer::new(*ratio, sdram)) as Box<dyn Patch>
         };
-        let k = Box::new(KnobGain::new(index));
+        let k = Box::new(KnobGain::new(*knob_num));
         let s = Box::new(Seq::new(BLOCK_SIZE, h, k));
         harms.push(MixerChannel(1.0, s));
     }
+    harms.push(MixerChannel(1.0, Box::new(PassThruFilter::new())));
     Box::new(Mixer::new(harms))
 }
